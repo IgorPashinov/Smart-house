4af803e2c4e48635869fb4b682bffd6745c888a8
Vansy115 <dimas.rovnyago@mail.ru>
2020-08-08 18:40:02 +0700

    Добавил фрагмент для света и энергосбережения

--- app/src/main/java/com/example/myapplication/ui/DostupFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/DostupFragment.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.myapplication.R
 
-class ClimateFragment: Fragment() {
+class DostupFragment: Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
