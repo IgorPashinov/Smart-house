c4ac8ff60bdee31b150a529cc426549c0a0277a0
Vansy115 <dimas.rovnyago@mail.ru>
2020-08-10 14:18:48 +0700

    Добавил фрагмент для света и энергосбережения

---
+++ app/src/main/java/com/example/myapplication/web/ApiService.kt
@@ -0,0 +1,28 @@
+package com.example.myapplication.web
+
+import com.example.myapplication.data.DataIlumination
+import com.example.myapplication.data.DataLightAll
+import com.example.myapplication.data.Turnoforturnon
+import retrofit2.http.GET
+import retrofit2.http.POST
+
+interface ApiService {
+
+    @GET("get/light/turn_of_or_turn_on")
+    suspend fun getlightturnoforturnon(): Turnoforturnon
+
+    @POST("set/light/turn_of_or_turn_on")
+    suspend fun setlightturnoforturnon(state: Turnoforturnon)
+
+    @GET("get/light/DataIllumination")
+    suspend fun getlightDataIllumination(): DataIlumination
+
+    @POST("set/light/turn_of_or_turn_on")
+    suspend fun setlightturnoforturnon(state: DataIlumination )
+
+
+
+    @GET("get/light/history")
+    suspend fun getLightHistory(): DataLightAll
+
+}
